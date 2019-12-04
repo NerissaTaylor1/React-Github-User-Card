28933b5eb50d27743f967c75dff090bbd1a9fa46
Nerissa Taylor <taylornerissa@hotmail.com>
2019-12-04 00:36:19 -0500

    got followers to work put in separate component

--- reactgithubusercard/src/App.js
+++ reactgithubusercard/src/App.js
@@ -2,44 +2,44 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import Users from "./components/Users";
-import Followers from "./components/Followers";
+import FollowerList from "./components/FollowerList";
 import GitHubCalendar from 'react-github-calendar';
 
 class App extends React.Component {
   state = {
     userData: {},
     followers: [],
-    username: "nerissaj"
+    username: "nerissaj",
+    _isMounted: false
 
   }
+
   componentDidMount() {
+    this._isMounted = true;
+
     axios.get(`https://api.github.com/users/${this.state.username}`)
       .then(res => {
-        // console.log(res.data);
-        this.setState({ userData: res.data })
-      })
-
-    // componentDidUpdate(prevProps, prevState) {
-    //   if (prevState.followers !== this.state.followers) {
-    //     console.log("new followers");
-
-    axios.get(`https://api.github.com/users/${this.state.username}/followers`)
-      .then(res => {
-        console.log(res.data);
-        this.setState({ followers: res.data })
+        if (this._isMounted) {
+          // console.log(res.data);
+          this.setState({ userData: res.data });
+        }
       });
+  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   
+  componentWillUnmount() {
+    this._isMounted = false;
 
   }
 
   render() {
     return (
       <div className="App">
+
         <GitHubCalendar username={`${this.state.username}`} fullYear={false} />
 
         <Users user={this.state.userData} />
-        {/* <Followers followers={this.state.followers} /> */}
-
-
+        <FollowerList />
       </div>
     );
   }
--- reactgithubusercard/src/components/Followers.js
+++ reactgithubusercard/src/components/Followers.js
@@ -1,15 +1,7 @@
-// import React from 'react'
+// import React from "react";
 
-// export default function Followers(props) {
-//     return (
-//         <div>
-//             {props.followers.map(follower => {
-//                 return(
-//                 <div key={props.follower.id}>
+// const Followers = props => {
+//     console.log(props)
 
-//            <img src={props.follower.avatar_url} />
-//                     <h1>{props.follower.login}</h1>
-//                 })
-//                 </div>
-//     )
-//             }
+// }
+// export default Followers;
